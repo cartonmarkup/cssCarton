2542a116d96057c058cdf07e8626b8ea32d24e2c
Mathias Prinz <mathias_prinz@me.com>
2015-10-14 10:39:53 +0200

    mayor bower changes

---
+++ samples/gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		connect: {
+			server: {
+				options: {
+					livereload: false,
+					port: 9001,
+					base: '../samples'
+				}
+			}
+		},
+		
+		less: {
+			development: {
+				options: {
+					compress: false,
+					yuicompress: true,
+					optimization: 2
+				},
+				files: {
+					"carton.css": "carton.less"
+				}
+			}
+		},
+		watch: {
+			styles: {
+				files: ['*.less', '../*.less'],
+				tasks: ['less'],
+				options: {
+					nospawn: true
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['less', 'connect:server', 'watch' ]);
+};
